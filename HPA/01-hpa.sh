0594566311f0f9f80dc50f428191794c189e84b9
awsandy <awsandy@amazon.co.uk>
2020-10-06 20:48:26 +0100

    HPA

---
+++ HPA/01-hpa.sh
@@ -0,0 +1,25 @@
+# create the metrics-service namespace first
+kubectl create namespace metrics
+helm repo update
+helm install metrics-server \
+    stable/metrics-server \
+    --version 2.11.1 \
+    --namespace metrics
+
+#kubectl describe hpa
+echo "check"
+kubectl get --raw "/apis/metrics.k8s.io/v1beta1/nodes"
+kubectl get apiservice v1beta1.metrics.k8s.io -o json
+kubectl get apiservice v1beta1.metrics.k8s.io -o json | jq .status.conditions[].message
+echo "deploy sample app"
+kubectl create -f php-apache.yaml
+echo "create an hpa resource"
+kubectl autoscale deployment php-apache --cpu-percent=50 --min=1 --max=10
+kubectl get hpa
+sleep 60
+kubectl get hpa
+kubectl run -i --tty load-generator --image=busybox /bin/sh
+echo "while true; do wget -q -O - http://php-apache; done"
+kubectl get hpa -w
+
+
