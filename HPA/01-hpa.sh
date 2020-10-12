db77c3e967936c9ec764f640c193c8fe51c13074
awsandy <awsandy@amazon.co.uk>
2020-10-12 17:23:05 +0100

    hpa

--- HPA/01-hpa.sh
+++ HPA/01-hpa.sh
@@ -14,11 +14,11 @@ kubectl get apiservice v1beta1.metrics.k8s.io -o json | jq .status.conditions[].
 echo "deploy sample app"
 kubectl create -f php-apache.yaml
 echo "create an hpa resource"
-kubectl autoscale deployment php-apache --cpu-percent=50 --min=1 --max=10
+kubectl autoscale deployment php-apache --cpu-percent=50 --min=1 --max=20
 kubectl get hpa
 sleep 60
 kubectl get hpa
-kubectl run -i --tty load-generator --image=busybox /bin/sh
+echo "kubectl run -i --tty load-generator --image=busybox /bin/sh"
 echo "while true; do wget -q -O - http://php-apache; done"
 kubectl get hpa -w
 
